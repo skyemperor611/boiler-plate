a4f766bf32137b6c557345aa6166d198b10e6e14
skyemperor611 <blackmonarch124@gmail.com>
2021-02-09 02:30:04 +0900

    final

---
+++ client/src/hoc/auth.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+import React, {useEffect} from 'react';
+import Axios from 'axios';
+import {useDispatch} from 'react-redux';
+import {auth} from '../_actions/user_action';
+
+export default function (SpecificComponent, option, adminRoute = null) {
+    
+    function AuthenticationCheck(props) {
+
+        const dispatch = useDispatch
+
+        useEffect(() => {
+
+            dispatch(auth()).then(response => {
+                console.log(response)
+
+                if(!response.payload.isAuth) {
+                    if(option) {
+                        props.history.push('/login')
+                    }
+                } else {
+                    if(option === false){
+                        props.history.push('/')
+                    }
+                }
+
+            })
+           Axios.get('/api/users/auth')
+        }, [])
+
+        return (
+            <SpecificComponent />
+        )
+
+    }
+    
+    return AuthenticationCheck
+}
